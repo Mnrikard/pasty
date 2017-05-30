8976b07e42b60237e5724a5f6046411c42fe15d8
Mnrikard <mnrikard@gmail.com>
2017-05-30 09:03:09 -0400

    better vb, still working on c formatting

--- editors/formatter.js
+++ editors/formatter.js
@@ -21,9 +21,10 @@ exports.oneLiner = "indents your code";
 
 var str = require("../stringHelpers.js");
 
+var tabstr = "tab";
 
-function tabRightC(code)
-{
+
+function tabRightC(code) {
 	var tabcount = 0;
 	var keywords = ["abstract","as","base","break","case","catch","checked","continue","default","delegate","do","else","event","explicit","extern","false","finally","fixed","for","foreach","goto","if","implicit","in","interface","internal","is","lock","namespace","new","null","object","operator","out","override","params","private","protected","public","readonly","ref","return","sealed","sizeof","stackalloc","switch","this","throw","true","try","typeof","unchecked","unsafe","using","virtual","while"];
 	var types = [ "bool", "byte", "char", "class", "const", "decimal", "double", "enum", "float", "int", "long", "sbyte", "short", "static", "string", "struct", "uint", "ulong", "ushort", "void" ];
@@ -35,60 +36,52 @@ function tabRightC(code)
 	var prevChar = "";
 	var setPrvChr = "";
 
-	var obt = require("../settings.js").settings;
-	var tabstr = obt.tabString;
-
 	var sr = code.split(/\r*\n/g);
-	var output = "";
+	var output = [];
 	var i;
 
 	sr.forEach(function(line){
 		var subtracttab = 0;
-		var buildLine = "";
 		if(!blockString && !blockComment){
 			line = line.trim();
 		}
+
 		prevChar = os.EOL;
+		var c;
+		var concurrentQuot;
+		var concurrentSlash;
 		for (i = 0; i < line.length; i++ ){
-			var c = line[i];
+			c = line[i];
 			setPrvChr = c;
 
 			//strings
 			if (c === '"' || blockString){
-				buildLine += c;
 				inString = true;
 				if (prevChar === "@"){
 					blockString = true;
-					var concurrentQuot = 0;
+					concurrentQuot = 0;
 					while (++i < line.length){
 						if (line[i] === '"'){
 							concurrentQuot++;
-							buildLine += line[i];
 						} else if (concurrentQuot > 0) {
-							if (concurrentQuot % 2 == 1) {
+							if (concurrentQuot % 2 === 1) {
 								i--;
 								inString = false;
 								blockString = false;
 								break;
-							} else {
-								buildLine += line[i];
 							}
 							concurrentQuot = 0;
-						} else {
-							buildLine += line[i];
 						}
 					}
 				} else {//simple string
-					var concurrentSlash = 0;
+					concurrentSlash = 0;
 					
 					while (++i < line.length) {
-						buildLine += line[i];
-
 						if (line[i] === "\\"){
 							concurrentSlash++;
 						} else {
-							if(line[i] == '"') {
-								if (concurrentSlash % 2 == 0) {
+							if(line[i] === '"') {
+								if (concurrentSlash % 2 === 0) {
 									inString = false;
 									break;
 								}
@@ -100,8 +93,7 @@ function tabRightC(code)
 			}
 
 			//Block comments
-			else if ((prevChar=="/" && c == '*') || blockComment) {
-				buildLine += c;
+			else if ((prevChar==="/" && c === '*') || blockComment) {
 				if(!blockComment) {
 					i++;
 					prevChar = "*";
@@ -109,9 +101,7 @@ function tabRightC(code)
 				inComment = true;
 				blockComment = true;
 				while (++i < line.length) {
-					buildLine += line[i];
-
-					if (line[i] == '/' && prevChar == "*") {
+					if (line[i] === '/' && prevChar === "*") {
 						inComment = false;
 						blockComment = false;
 						break;
@@ -121,22 +111,18 @@ function tabRightC(code)
 			}
 
 			//comments
-
-			else if (prevChar=="/" && c == '/') {
-				buildLine += c;
-				while (++i < line.Length) {
+			else if (prevChar==="/" && c === '/') {
+				while (++i < line.length) {
 					buildLine += line[i];
 				}
 			}
 
-			else if (c == '{' && !inComment && !inString) {
-				buildLine += c + os.EOL;
+			else if (c === '{' && !inComment && !inString) {
+				buildLine += c;
 				tabcount++;
-			} else if (c == '}' && !inComment && !inString) {
+			} else if (c === '}' && !inComment && !inString) {
 				tabcount--;
-				buildLine += `${os.EOL}${str.makeString(tabstr, tabcount)}${c}${os.EOL}`;
-			} else if (c == ';' && !inComment && !inString) {
-				buildLine += `${c}${os.EOL}`;
+				buildLine += `${str.makeString(tabstr, tabcount)}${c}`;
 			} else {
 				buildLine += c;
 			}
@@ -148,15 +134,105 @@ function tabRightC(code)
 			subtracttab = tabcount * -1;
 		}
 
-		output += `${str.makeString(tabstr,tabcount+subtracttab)}${buildLine}${os.EOL}`;
+		output.push(`${str.makeString(tabstr,tabcount+subtracttab)}${buildLine}`);
 	});
 
-	//output = Regex.Replace(output, "\n+", "\n");
+	return output.join(os.EOL);
+}
 
-	return output;
+RegExp.prototype.IsMatch = function(teststr){
+	this.lastIndex = 0;
+	return this.test(teststr);
+};
+
+function tabRightVb(code){
+	code = code.replace(/_\s*\n\s*/g," ");
+	code = code.replace(/(else)\s*('.+)/gi, "$1\n$2");
+	code = code.replace(/(then)\s*('.+)/gi, "$1\n$2");
+
+	var tplus = new RegExp("^(while|for|if|elseif|class|function|sub|select\\s+case|do|(private\\s+|public\\s+|friend\\s+|protected\\s+)?(shared\\s+|mustinherit\\s+|sealed\\s+)?(function|class|sub|property|module))[\\s]", "gi");
+	var singleLineIf = new RegExp("then$", "gi");
+	var tabBecauseIf = new RegExp("^if", "gi");
+	var tminus = new RegExp("^(wend|until|loop|next|elseif|end\\s+(if|function|sub|class|select|property))[\\s]?", "gi");
+	var isLabel = new RegExp("[^\\s]:$","g");
+	var elif = new RegExp("^elseif.+then$", "gi");
+
+	//special instructions for "select case" statements
+	var casebound = new RegExp("^(select case|end select)", "gi");
+	var caseitem = new RegExp("^case\s", "gi");
+
+	var incase = false;
+
+	//get rid of multiline rows
+	code = code.replace(/_[ \t]+\n[ \t]*/i, " ");
+
+	var rows = code.split(/\n/g);
+	var ii = 0;
+	var tabcount=0;
+
+	for (ii = 0; ii < rows.length; ii++) {
+		var currTabc = 0;
+
+		rows[ii] = rows[ii].trim();
+
+		if (casebound.IsMatch(rows[ii])) {
+			if (incase) {
+				tabcount--;
+				incase = false;
+			} else {
+				incase = true;
+				tabcount++;
+				//I know I'm doubling the tab count, I want to
+			}
+		}
+
+		if (tminus.IsMatch(rows[ii]) && !isLabel.IsMatch(rows[ii])) {
+			tabcount--;
+		}
+
+		if (tabcount < 0) {
+			tabcount = 0;
+		}
+		currTabc = tabcount;
+
+		var currentLine = "";
+		if (isLabel.IsMatch(rows[ii])) {
+			currentLine = rows[ii];
+		} else if (incase && caseitem.IsMatch(rows[ii]) && tabcount > 0) {
+			currentLine = str.makeString(tabstr, tabcount-1) + rows[ii];
+		} else if (rows[ii].toLowerCase().trim() == "else" && tabcount > 0) {
+			currentLine = str.makeString(tabstr, tabcount - 1) + rows[ii];
+		} else {
+			currentLine = str.makeString(tabstr, currTabc) + rows[ii];
+		}
+
+		if (tplus.IsMatch(rows[ii]) && !isLabel.IsMatch(rows[ii])) {
+			console.log("tplus");
+			if (tabBecauseIf.IsMatch(rows[ii])) {
+				if (singleLineIf.IsMatch(rows[ii])) {
+					tabcount++;
+				}
+			} else {
+				tabcount++;
+			}
+		}
+		currTabc = tabcount;
+		if (currTabc < 0) { currTabc = 0; }
+		rows[ii] = currentLine;
+	}
+	return rows.join(os.EOL).replace(/(\t|    )(Select\s+Case)/gi,"$2");
 }
 
 exports.edit=function(input, switches){
-	return tabRightC(input);
+	var obt = require("../settings.js").settings;
+	tabstr = obt.tabString;
+
+	if(exports.parms[0].value.trim().toLowerCase() === "c"){
+		return tabRightC(input);
+	}
+
+	if(exports.parms[0].value.trim().toLowerCase() === "vb"){
+		return tabRightVb(input);
+	}
 };
 
