6976452a5eca3699f189499284f5839f5e2c0729
Matthew Rikard <mrikard@geninfo.com>
2017-06-19 22:29:00 -0400

    persisting window for errors

--- index.js
+++ index.js
@@ -8,20 +8,24 @@ var editorRunner = require("./editorRunner.js");
 
 debugger;
 if(process.stdin.isTTY){
-	var content = "";
-	var clipboard = require("clipboardy");
 	try{
-		content = clipboard.readSync();
-	} catch(e) {
-		clipboard.writeSync(content);
-		console.log(e);
+		var content = "";
+		var clipboard = require("clipboardy");
+		try{
+			content = clipboard.readSync();
+		} catch(e) {
+			clipboard.writeSync(content);
+		}
+		var newContent = editorRunner.handleInput(content, args);
+		clipboard.writeSync(newContent);
+	} catch(err) {
+		console.log(err);
 		require("./stringHelpers.js").keepWindowOpen();
 	}
-	var newContent = editorRunner.handleInput(content, args);
-	clipboard.writeSync(newContent);
 	process.exit();
 } else {
 	editorRunner.interactive = false;
+	require("./stringHelpers.js").keepWindowOpen = function(){};
 	var pipedInput = '';
 	process.stdin.on('readable', function() {
 		var chunk = this.read();
