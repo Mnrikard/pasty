dd76abab46d1e51590644c8854ab0de24f7ed86a
Matthew <mnrikard@gmail.com>
2019-11-08 15:58:10 -0500

    expands directories and doesn't fail if your pluginsDirectory does not exist

--- settings.js
+++ settings.js
@@ -5,12 +5,19 @@ function getSettings(){
 	var settingsPath = getSettingsFile();
 	if(fs.existsSync(settingsPath)){
 		var settings = fs.readFileSync(settingsPath);
-		return JSON.parse(settings);
+		var output = JSON.parse(settings);
+		fullyQualifyHomeDirectory(output);
+		return output;
 	} else {
 		return {"tabString":"\t"};
 	}
 	//if file exists, consume and return the object
 	//otherwise return the default object
+}
+
+function fullyQualifyHomeDirectory(settingsObject){
+	settingsObject.pluginsDirectory = settingsObject.pluginsDirectory.replace("~", os.homedir());
+	settingsObject.localStyleSheet = settingsObject.localStyleSheet.replace("~", os.homedir());
 }
 
 function getSettingsFile(){
