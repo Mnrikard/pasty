1c62560396b6fa82a8907a8484666dfccab5c1e1
Matthew Rikard <Mnrikard@users.noreply.github.com>
2026-02-02 20:46:46 -0500

    runnable from run box

--- main.go
+++ main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+ "os"
+	"os/exec"
+	"runtime"
+	"golang.org/x/term"
 	"github.com/mattr/pasty/cmd"
 	"github.com/spf13/cobra"
 )
 
 func main() {
-	cobra.EnableCaseInsensitive = true
+	if runtime.GOOS == "windows" && !term.IsTerminal(int(os.Stdout.Fd())) {
+		cmd := exec.Command("cmd", "/k", os.Args[0])
+		cmd.Run()
+		return 
+	}
+
+ cobra.EnableCaseInsensitive = true
 	cmd.Execute()
 }
