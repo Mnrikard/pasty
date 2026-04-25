7cdfb9b00fa374a98d95509fd3e9841623b9be2d
Matthew Rikard <mnrikard@gmail.com>
2026-04-24 23:22:19 -0400

    removing useless attempt at handling cobra console requirement

--- main.go
+++ main.go
@@ -1,21 +1,11 @@
 package main
 
 import (
- "os"
-	"os/exec"
-	"runtime"
-	"golang.org/x/term"
 	"github.com/mattr/pasty/cmd"
 	"github.com/spf13/cobra"
 )
 
 func main() {
-	if runtime.GOOS == "windows" && !term.IsTerminal(int(os.Stdout.Fd())) {
-		cmd := exec.Command("cmd", "/k", os.Args[0])
-		cmd.Run()
-		return 
-	}
-
- cobra.EnableCaseInsensitive = true
+	cobra.EnableCaseInsensitive = true
 	cmd.Execute()
 }
